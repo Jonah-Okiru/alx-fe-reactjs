b9e1d6ae68fcecc7a8111da485d589dbba92c432
Jonah-Okiru <okirujonah@gmail.com>
2024-11-07 18:05:47 +0300

    intitial commit of alx-react-app-props

---
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserDetails() {
+    const data = useContext(UserContext)
+    return (
+      <div>
+        {data}
+      </div>
+    );
+  }
+  
+  export default UserDetails;
