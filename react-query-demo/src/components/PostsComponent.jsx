118f25ae84631f991384ee8618f7ec03580e3f7c
Jonah-Okiru <okirujonah@gmail.com>
2024-11-23 10:48:15 +0300

    update on PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -11,11 +11,20 @@ const fetchPosts = async () =>{
 };
 const PostsComponent = () => {
     // use the query hook to handle data fetching and caching
-    const {data, isError, isLoading} = useQuery('fetchPosts', fetchPosts);
+    const {data, error, isError, isLoading} = useQuery(
+        'fetchPosts', 
+        fetchPosts,
+        {
+            cacheTime: 5*60*1000, // cache data for 5 minutes
+            staleTime: 1*60*1000, // Consider data refresh for one minute
+            refetchOnWindowFocus: false, // disable refetching on window focus
+            keepPreviousData: true // keep previous data while refetching new data
+
+        });
     // handle loading state
     if (isLoading) return <div>Loading...</div>
     // handle error state
-    if (isError) return <div>Error loading: {isError.message}</div>
+    if (isError) return <div>Error loading: {error.message}</div>
     // Ensure data is an array before mapping
     if (!Array.isArray(data)) {
         return <div>No data available</div>;
