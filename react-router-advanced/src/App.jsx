bd5538caf0f9f3671ff46f957c2b82fd072f05e4
Jonah-Okiru <okirujonah@gmail.com>
2024-11-23 13:36:09 +0300

    update on ProtectedRoute.jsx file

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import BlogPost from './components/BlogPost';
-import ProtectedRoute from './ProtectedRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css'
 
 function App() {
