792386cbaf7326f684a4e8a8fe84babb21521cf1
Jonah-Okiru <okirujonah@gmail.com>
2024-12-03 16:17:21 +0300

    update on the Hompage file

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+function HomePage (){
+    // set the initial state of the data
+    const [recipes, setRecipes] = useState([]);
+    // Fetching the data from json file and updating the state of the data from an empty array to annray containg the data
+    useEffect(()=>{
+        fetch('/data.json').then((respose) =>{
+            if (!respose.ok){
+                throw new Error('Failed to fetch recipes');
+                
+            }
+            return respose.json();
+        })
+        .then((data) =>{
+            setRecipes(data);
+        })
+        .catch((error) =>{
+            console.error("Error fetching the recipe data", error);
+        })
+        
+        
+    }, []);
+    return (
+        <div className="sm:640px md:768px lg:1024px">
+            <h1 className="text-blue-700 text-center font-bold pb-3">Recipe list</h1>
+            {
+                recipes.map((recipe)=> (
+                    <div key={recipe.id} className="bg-pink-200 pt-3">
+                        <img src={recipe.image} className="mx-auto rounded-full h-40 w-40 sm:h-24 sm:w-24 md:h-50 md:w-50 hover:scale-110 transition-transform ease-in-out" alt="Image"/>
+                        <p className="text-black font-bold decoration-2">{recipe.title}</p>
+                        <p className="pb-3 font-semibold">{recipe.summary}</p>
+                    </div>
+                )
+                )
+            }
+        </div>
+    )
+
+
+};
+export default HomePage;
