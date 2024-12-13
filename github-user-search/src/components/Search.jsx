a36e69c3259b9e027b3ff7a4178a6930239f7279
Jonah-Okiru <okirujonah@gmail.com>
2024-12-13 16:18:59 +0300

    update on search.jsx files

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,25 +1,66 @@
 import React, {useState} from 'react';
+
 const Search = ({onSearch}) =>{
+    const [result, setResult] = useState(null);
     const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const handleInputChange = (e) => {
         setQuery(e.target.value);
     };
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        if (query.trim()){
-            onSearch(query);
+    
+    };
+    const handleSearch = async (query) =>{
+        setLoading(true);
+        setError(null);
+        setResult(null);
+        try{
+            const userData = await fetchUserData(query);
+            setResult(userData);
+        } catch (err){
+            setError("Looks like we can't find the user");
+        } finally{
+            setLoading(false);
         }
     };
     return (
-        <form onSubmit={handleFormSubmit}>
-            <input 
-                type ="text"
-                placeholder = "Search github users"
-                onChange={handleInputChange}
-            />
-            <button type="submit">Search</button>
+        <div>
+            <h>Github user search</h>
+            <form onSubmit={handleFormSubmit}>
+                <input 
+                    type ="text"
+                    placeholder = "Search github users"
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Search</button>
 
-        </form>
+            </form>
+            <div>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            result && (
+                <div>
+                    <img 
+                        src={result.avatar_url}
+                        alt={`${result.login} avatar`}
+                    />
+                    <h2>{result.login}</h2>
+                    <p>
+                        <a 
+                            href={result.html_url}
+                            target="_blank"
+                            rel ="noopener no referrer"
+                        >
+                            Visit Profile
+                        </a>
+                    </p>
+                </div>
+            )
+            </div>
+        </div>
+        
     );
 };
 export default Search;
